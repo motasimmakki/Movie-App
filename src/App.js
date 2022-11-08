8d5358399885cf5f105eb80930abeb5477b1832d
motasimmakki <makkimotasim@gmail.com>
2022-11-08 13:17:02 +0530

    :art:feat: Formated Banner

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Navbar from './Components/Navbar';
 import './Components/Navbar.css'
 import Banner from './Components/Banner'
 import './Components/Banner.css'
+import List from './Components/List'
+import './Components/List.css'
 
 function App() {
   return (
     <>
       <Navbar/>
       <Banner/>
+      <List/>
     </>
   );
 }
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -6,11 +6,11 @@ export default class Banner extends Component {
     let movie = movies.results[0];
     return (
       <div className='banner-cont'>
-        <div className="card">
-            <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} class="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{movie.original_title}</h5>
-                <p className="card-text">{movie.overview}</p>
+        <div className="card banner-card">
+            <img className="card-img-top banner-img" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="..."/>
+            <div className="card-body banner-body">
+                <h5 className="card-title banner-title">{movie.original_title}</h5>
+                <p className="card-text banner-desc">{movie.overview}</p>
                 {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
         </div>
