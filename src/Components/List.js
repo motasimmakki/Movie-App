bafe97304364b5523245f7039c8c4bde15d9a450
motasimmakki <makkimotasim@gmail.com>
2022-11-08 16:08:14 +0530

    :technologist:feat: Rendering all movies from getMovies.js

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import {movies} from './getMovies'
 
 export default class List extends Component {
   render() {
+    let allMovies = movies.results;
     return (
-      <div>List</div>
+      <div>
+        <h3 className='trending'>Trending</h3>
+        <div className='movies-list'>
+            {
+            allMovies.map(movie => {
+                return (
+                    <div className="card movie-card">
+                        <img className="card-img-top movie-img" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="..."/>
+                        <h5 className="card-title movie-title">{movie.original_title}</h5>
+                        <div className='movie-btn-wrapper'>
+                            <a href="#" className="btn btn-primary movie-btn">Add To Favourites</a>
+                        </div>
+                    </div>
+                );
+            })
+            }
+        </div>
+      </div>
     )
   }
 }
