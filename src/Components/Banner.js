7a511800b20d711f2d22e97b8c8c5462f34ebd56
motasimmakki <makkimotasim@gmail.com>
2022-11-08 12:09:11 +0530

    :zap:feat: Added Navabar

---
+++ src/Components/Banner.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Banner extends Component {
+  render() {
+    return (
+      <div className='banner-cont'>
+        {/* <div className="card">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Pirates Of The Caribbean</h5>
+                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div> */}
+      </div>
+    )
+  }
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className='navbar-cont'>
+        <h1>Movies App</h1>
+        <h2>Favourites</h2>
+      </div>
+    )
+  }
+}
