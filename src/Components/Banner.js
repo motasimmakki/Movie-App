7bcb37e99ca88aac2d2be35b4e8373a23e530d16
motasimmakki <makkimotasim@gmail.com>
2022-11-08 12:46:20 +0530

    :technologist:feat: Added Static banner poster with desc.

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
+import {movies} from './getMovies';
 
 export default class Banner extends Component {
   render() {
+    let movie = movies.results[0];
     return (
       <div className='banner-cont'>
-        {/* <div className="card">
-            <img src="..." class="card-img-top" alt="..."/>
+        <div className="card">
+            <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} class="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Pirates Of The Caribbean</h5>
-                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{movie.original_title}</h5>
+                <p className="card-text">{movie.overview}</p>
+                {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
-        </div> */}
+        </div>
       </div>
     )
   }
