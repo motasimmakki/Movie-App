25c88bef5ce2450fd6dc550f7dbe08d4096cc04b
motasimmakki <makkimotasim@gmail.com>
2022-11-12 17:33:22 +0530

    :technologist:feat: Extracted genre as string & active feature

--- src/Components/Favourites.js
+++ src/Components/Favourites.js
@@ -7,23 +7,10 @@ export default class Favourites extends Component {
     super();
     this.state = {
       movies: [],
-      genres: []
+      genres: [],
+      currGenre: "All Genres"
     }
-  }
-  async componentDidMount() {
-    // Using Axios.
-    let data = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=fa7127a13c542d5323ce1a236b9df18a&language=en-US&page=1");
-    console.log(data.data);
-
-    this.setState({
-      movies: [...data.data.results],
-      genres: [...new Set([...data.data.results.map(movie => {
-        return movie.genre_ids;
-      })].flat())]
-    });
-  }
-  render() {
-    let genreId = {
+    this.genreId = {
       28: "Action",
       12: "Adventure",
       16: "Animation",
@@ -44,21 +31,38 @@ export default class Favourites extends Component {
       10752: "War",
       37: "Western",
     };
+  }
+  async componentDidMount() {
+    // Using Axios.
+    let data = await axios.get("https://api.themoviedb.org/3/movie/popular?api_key=fa7127a13c542d5323ce1a236b9df18a&language=en-US&page=1");
+    console.log(data.data);
+
+    let allGenre = [...data.data.results.map(movie => {
+      return this.genreId[movie.genre_ids[0]];
+    })];
+
+    this.setState({
+      movies: [...data.data.results],
+      genres: ["All Genres", ...new Set(allGenre)]
+    });
+  }
+  render() {
     return (
       <>
       <Navbar/>
       <div className='p-5 gap-5 favourites-cont'>
         <ul className="list-group col-3 favourites-list">
-          <li className="list-group-item active" aria-current="true">All Genres</li>
-          {
-            this.state.genres.map(currGenre => (
-              <li className="list-group-item">
-                {genreId[currGenre]}
-              </li>
-            ))
-          }
-          {/* <li className="list-group-item">Fantacy</li>
-          <li className="list-group-item">Animation</li> */}
+        {
+          this.state.genres.map(currGenre => (
+            (currGenre === this.state.currGenre)?
+            <li className="list-group-item active">
+              {currGenre}
+            </li>
+            : <li className="list-group-item">
+              {currGenre}
+            </li>
+          ))
+        }
         </ul>
         <div className='row favourites-table'>  
           <div className='d-flex justify-content-between favourites-search'>
@@ -83,7 +87,7 @@ export default class Favourites extends Component {
                       <img className='movie-img me-3' src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}/>
                       <span>{movie.original_title}</span>
                     </td>
-                    <td className='align-middle'>{genreId[movie.genre_ids[0]]}</td>
+                    <td className='align-middle'>{this.genreId[movie.genre_ids[0]]}</td>
                     <td className='align-middle'>{movie.popularity}</td>
                     <td className='align-middle'>{movie.vote_average}</td>
                     <td className='align-middle'><button className='btn btn-outline-danger'>DELETE</button></td>
